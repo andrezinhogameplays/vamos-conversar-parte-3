f71787fd2e737d9bb109029ebc3b4002f3e6990a
andrezinhogameplays <137837130+andrezinhogameplays@users.noreply.github.com>
2024-05-03 21:51:05 -0300

    Add files via upload

---
+++ kwitter.css
@@ -0,0 +1,2 @@
+<link rel="stylesheet" type="text/css" href="style/style.css" >
+<script src="kwitter.js></script>
---
+++ kwitter_page.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB5XEf-PKaIXV2Rs5GRq2bSuJXwJV2Gyro",
+    authDomain: "app-conversar-1215f.firebaseapp.com",
+    projectId: "app-conversar-1215f",
+    storageBucket: "app-conversar-1215f.appspot.com",
+    messagingSenderId: "368359491279",
+    appId: "1:368359491279:web:44e7a116c58cf3478259ba"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
